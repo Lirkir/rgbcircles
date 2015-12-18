e5a2134d45c2ff453d7f500e8b51c2da5f63c89e
https://github.com/Lirkir/lab22.git <https://github.com/Lirkir/lab22.git>
2015-12-18 08:00:20 +0300

    Добавление цвета.

--- src/ru/khasang/rgbcircles/MainCircle.java
+++ src/ru/khasang/rgbcircles/MainCircle.java
@@ -1,14 +1,18 @@
 package ru.khasang.rgbcircles;
 
+import android.graphics.Color;
+
 /**
  * Created by kiril on 11.12.2015.
  */
 public class MainCircle extends SimpleCircle{
     public static final int INIT_RADIUS = 50;
     public static final int MAIN_SPEED = 30;
+    public static  final int OUR_COLOR = Color.BLUE;
 
     public MainCircle(int x, int y) {
         super(x, y, INIT_RADIUS);
+        setColor(OUR_COLOR);  // цвет шара игрока
     }
 
     public void moveMainCircleWhenTouchAt(int x1, int y1) {
